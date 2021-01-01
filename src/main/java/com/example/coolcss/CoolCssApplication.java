b09071fba65b2114781341672eb6936f8b62737f
FLY <610841614@qq.com>
2021-01-01 21:15:49 +0800

    Initial commit

---
+++ src/main/java/com/example/coolcss/CoolCssApplication.java
@@ -0,0 +1,13 @@
+package com.example.coolcss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolCssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoolCssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coolcss/CoolCssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coolcss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolCssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
